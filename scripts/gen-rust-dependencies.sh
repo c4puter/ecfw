20c948ae5c73facf9cf2df7923ee3251788ddb68
Chris Pavlina <pavlina.chris@gmail.com>
2016-09-19 20:36:42 -0400

    Rust dep script: prune better after move to resources/

--- scripts/gen-rust-dependencies.sh
+++ scripts/gen-rust-dependencies.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-EXCLUDES="-name rustsrc -o -name asf -o -name asf-unf"
-
 find_header() {
-    find . \( $EXCLUDES -prune \) -o \( -name "$1" -print \) |
+    find . -name resources -prune -o \( -name "$1" -print \) |
         awk '{n=gsub("/","/",$0); printf "%04d/%s\n",n,$0}' |
         sort -t/ |
         sed 's|[^/]*/||'
 }
 
-find . -name rustsrc -prune -o \( -name '*.rs' -print0 \) | while read -d $'\0' srcfile; do
+find . -name resources -prune -o \( -name '*.rs' -print0 \) | while read -d $'\0' srcfile; do
 
     echo -n "$srcfile:"
     grep 'extern crate' "$srcfile" | awk '{print $3}' | sed 's/;//' | while read dep; do
 
         without_bindgen="$(echo "$dep" | sed -e 's/^bindgen_//')"
-        location_rust="$(find . \( $EXCLUDES -prune \) -o \( -name "${dep}.rs" -print -quit \) | head -n 1)"
+        location_rust="$(find . -name resources -prune -o \( -name "${dep}.rs" -print -quit \) | head -n 1)"
         location_c="$(find_header "${without_bindgen}.h" | head -n 1)"
 
         if [[ x"$location_rust" != x"" ]]; then
