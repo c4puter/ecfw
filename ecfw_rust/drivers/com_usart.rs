629c68d8aad7e87396cf833f193e706223dc457c
Chris Pavlina <pavlina.chris@gmail.com>
2017-12-05 08:16:28 -0700

    Dammit bindgen

--- ecfw_rust/drivers/com_usart.rs
+++ ecfw_rust/drivers/com_usart.rs
@@ -42,7 +42,7 @@ queue_static_new! {
 /// COM over local USART (for debug, to RS232 on board)
 pub static COMUSART: ComUsart = ComUsart {
     usart: USART1,
-    irqn: asf_usart::IRQn::USART1_IRQn as i32,
+    irqn: asf_usart::IRQn_USART1_IRQn as i32,
     queue_in: &QUEUE_IN_1,
     queue_out: &QUEUE_OUT_1,
 };
